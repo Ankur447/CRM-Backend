e43932df52609fcd5dcc3b06492a13ea30951679
Ankur <ankurbahadure007@gmail.com>
2025-01-02 19:41:21 +0530

    nothin

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const userRoutes = require('./routes/userRoutes'); // Combined routes
 const errorHandler = require('./middleware/errorMiddleware');
 require('dotenv').config();
 const cors = require('cors');
-const path = require('path');
-
 
 const app = express();
 
@@ -14,14 +12,7 @@ app.use(cors({
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }));
-
-// Serve static files from dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all route should be LAST
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// Use combined user routes
 
 app.use('/api/users', userRoutes);
 app.use(errorHandler);
