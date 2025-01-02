a8b7860216c1739814219449974def66f9c38843
Ankur <ankurbahadure007@gmail.com>
2025-01-02 18:43:25 +0530

    added FE Build

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes'); // Combined routes
 const errorHandler = require('./middleware/errorMiddleware');
+const path = require('path');
+
 require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Catch-all route to serve index.html for all non-API requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }));
