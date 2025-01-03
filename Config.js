c3b986684931513ccaab13690dcf65ab34da80cd
YashDabhade208 <yashdabhade208@gmail.com>
2025-01-03 17:21:13 +0530

    setup/config done for vercel

--- Config.js
+++ Config.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "crm",
+    database:"crm",
     waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS === 'true',
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT),
     queueLimit: parseInt(process.env.DB_QUEUE_LIMIT),
